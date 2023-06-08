90c7604c0d0eb8bcaaf4b2c1192fd400b2136dd6
Sergio Hernandez <ingenierosergiohdez@gmail.com>
2023-06-07 23:21:05 -0500

    primer commit

---
+++ src/javaapplication2/JavaApplication2.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication2;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author sergio
+ */
+public class JavaApplication2 extends Application{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch();
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("ejemplo");
+        stage.show();
+    }
+    
+}
